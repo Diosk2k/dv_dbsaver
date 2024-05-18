d55b0359da1ced43fc15ffacd7ec4bcc5eb4cbba
Diosk <diosk.dv@gmail.com>
2024-05-18 13:32:48 +0200

    Update linux_start_script.sh

--- linux_start_script.sh
+++ linux_start_script.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Install required Python modules for the script
+# Install mysql-connector-python
 pip install mysql-connector-python
 
+# Install configparser
 pip install configparser
 
+# Install requests
 pip install requests
 
+# Print a message when installation is completed
 echo "Required Python modules have been installed."
 
+# Check if the "backups" folder exists. If not, create the folder.
 if [ ! -d "backups" ]; then
     mkdir backups
     echo "Folder 'backups' has been created."
@@ -15,8 +21,11 @@ else
     echo "Folder 'backups' already exists."
 fi
 
+# Start the Python script
 python3 backup_script.py
 
+# Wait for user interaction before closing the window
 read -p "Press Enter to continue..."
 
+# Wait for 5 seconds to allow the user to see the message before closing the window
 sleep 5
